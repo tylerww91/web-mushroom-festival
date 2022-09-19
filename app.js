e502320fd24181788ae837e3deec70c5378adce2
Tyler Watson <tylerww91@gmail.com>
2022-09-19 14:05:45 -0700

    adds logic to the add friend button so that the form updates the friends array

--- app.js
+++ app.js
@@ -63,15 +63,20 @@ addFriendForm.addEventListener('submit', (e) => {
     // > create a new friend, with a "name" property that
     // is populated from `formData.get('name')` and a
     // "satisfied" property with an initial value of 0
-
+    const friend = {
+        name: formData.get('name'),
+        satisfied: 0,
+    };
     // > add the new friend to the friends array
-
+    friends.push(friend);
     // > set the message state to let the user know
     // they invited a new friend to the festival, include the friend's
     // name in the message
-
+    message = `${friend.name} has joined the hunt!`;
     addFriendForm.reset();
 
+    displayFriends();
+    displayMessage();
     // > call the display functions that need to re-display
 });
 
